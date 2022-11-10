29154b17488d37d60d19236a7cbea3939308c8ac
anhasamea <anhasamea707@gmail.com>
2022-11-10 12:59:24 +0600

    historical photos and data loaded from mongodb

--- index.js
+++ index.js
@@ -15,13 +15,27 @@ app.get('/', (req,res)=>{
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w75vwv7.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('ancientShot').collection('services');
+
+        //read
+        //all data load
+        app.get('/services',async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>{console.error(err)})
 
 app.listen(port,()=>{
     console.log(`Ancient Shot server is running on port ${port}`);
